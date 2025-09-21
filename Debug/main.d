1490eb6d98593cf2e92a40a393da50a7d9882ff0
Matthew Gomez - Morales <matthewgomezm10@gmail.com>
2025-09-21 14:29:40 -0400

    Add files via upload

---
+++ Debug/main.d
@@ -0,0 +1,168 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/driverlib.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h
+main.obj: /Applications/ti/ccs2020/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs2020/ccs/ccs_base/msp430/include/msp430fr5994.h
+main.obj: /Applications/ti/ccs2020/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Applications/ti/ccs2020/ccs/ccs_base/msp430/include/legacy.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+main.obj: /Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/esi.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/cs.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/aes256.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/uups.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/framctl_a.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/timer_b.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/framctl.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/tlv.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/timer_a.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/pmm.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/adc12_b.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/sysctl.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/sfr.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/ref_a.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/mpy32.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/wdt_a.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/saph.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/crc32.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/mpu.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/mtif.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/lcd_c.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/rtc_c.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/gpio.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/rtc_b.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/comp_e.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/ram.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/saph_a.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/dma.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/hspll.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/sdhs.h
+main.obj: /Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/crc.h
+
+../main.c:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/driverlib.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h:
+
+/Applications/ti/ccs2020/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs2020/ccs/ccs_base/msp430/include/msp430fr5994.h:
+
+/Applications/ti/ccs2020/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/Applications/ti/ccs2020/ccs/ccs_base/msp430/include/legacy.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+/Applications/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/esi.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/cs.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/aes256.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/uups.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/framctl_a.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/timer_b.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/framctl.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/tlv.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/timer_a.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/pmm.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/adc12_b.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/sysctl.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/sfr.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/ref_a.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/mpy32.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/wdt_a.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/saph.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/crc32.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/mpu.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/mtif.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/lcd_c.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/rtc_c.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/gpio.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/rtc_b.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/comp_e.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/ram.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/saph_a.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/dma.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/hspll.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/sdhs.h:
+
+/Users/matthewgomez/workspace_ccstheia/emptyProject/driverlib/MSP430FR5xx_6xx/crc.h:
+
